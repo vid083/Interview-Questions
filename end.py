b374b7299c0ebd9d0fcfb1f6d4ee14575b77124e
“vidya0397git config --global user.email “ramagirividya03@gmail.com <ramagirividya03@gmail.com>
2020-09-02 08:22:43 +0100

    Create end.py

---
+++ end.py
@@ -0,0 +1,15 @@
+#What is the purpose of “end” in Python?
+
+# Example: Print a  instead of the new line in the end.
+print("Let's learn" , end = ' ')  
+print("Python") 
+
+# Printing a dot in the end.
+print("Learn to code from techbeamers" , end = '.')  
+print("com", end = ' ')
+
+"""
+output:
+Let's learn Python
+Learn to code from techbeamers.com
+"""
